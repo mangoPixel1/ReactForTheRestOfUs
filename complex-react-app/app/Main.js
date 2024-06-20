ec7a8864d08bda9ce4699eb5b52a8cbca8a00991
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2024-06-19 17:49:20 -0700

    Set up React environment and VS Code settings

---
+++ complex-react-app/app/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function ExampleComponent() {
+    return (
+        <div>
+            <h1>This is our app!!!</h1>
+            <p>The sky is blue and the grass is green.</p>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<ExampleComponent />);
+
+if (module.hot) {
+    module.hot.accept();
+}
+// npm run dev
+// Ctr+C to stop
