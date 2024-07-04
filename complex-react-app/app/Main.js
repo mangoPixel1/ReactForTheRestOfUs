a91b1f14347676433d62bfd512cf6eab00806e09
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2024-07-03 17:48:55 -0700

    Create New Post, View Post

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
 // Components
 import Header from "./components/Header.js";
@@ -9,6 +11,8 @@ import Home from "./components/Home.js";
 import Footer from "./components/Footer.js";
 import About from "./components/About.js";
 import Terms from "./components/Terms.js";
+import CreatePost from "./components/CreatePost.js";
+import ViewSinglePost from "./components/ViewSinglePost.js";
 
 function Main() {
 	const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
@@ -18,6 +22,8 @@ function Main() {
 			<Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
 			<Routes>
 				<Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+				<Route path="/post/:id" element={<ViewSinglePost />} />
+				<Route path="/create-post" element={<CreatePost />} />
 				<Route path="/about-us" element={<About />} />
 				<Route path="/terms" element={<Terms />} />
 			</Routes>
