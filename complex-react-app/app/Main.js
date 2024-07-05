0653d93f70df40d30556bb900e2823070114b44b
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2024-07-04 18:14:21 -0700

    Flash messages and Context

--- complex-react-app/app/Main.js
+++ complex-react-app/app/Main.js
@@ -13,22 +13,32 @@ import About from "./components/About.js";
 import Terms from "./components/Terms.js";
 import CreatePost from "./components/CreatePost.js";
 import ViewSinglePost from "./components/ViewSinglePost.js";
+import FlashMessages from "./components/FlashMessages.js";
+import ExampleContext from "./ExampleContext.js";
 
 function Main() {
 	const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("complexappToken")));
+	const [flashMessages, setFlashMessages] = useState([]);
+
+	function addFlashMessage(msg) {
+		setFlashMessages(prev => prev.concat(msg));
+	}
 
 	return (
-		<BrowserRouter>
-			<Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
-			<Routes>
-				<Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
-				<Route path="/post/:id" element={<ViewSinglePost />} />
-				<Route path="/create-post" element={<CreatePost />} />
-				<Route path="/about-us" element={<About />} />
-				<Route path="/terms" element={<Terms />} />
-			</Routes>
-			<Footer />
-		</BrowserRouter>
+		<ExampleContext.Provider value={{ addFlashMessage, setLoggedIn }}>
+			<BrowserRouter>
+				<FlashMessages messages={flashMessages} />
+				<Header loggedIn={loggedIn} />
+				<Routes>
+					<Route path="/" element={loggedIn ? <Home /> : <HomeGuest />} />
+					<Route path="/post/:id" element={<ViewSinglePost />} />
+					<Route path="/create-post" element={<CreatePost />} />
+					<Route path="/about-us" element={<About />} />
+					<Route path="/terms" element={<Terms />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</ExampleContext.Provider>
 	);
 }
 
@@ -38,6 +48,8 @@ root.render(<Main />);
 if (module.hot) {
 	module.hot.accept();
 }
+
+// Frontend:
 // npm run dev
 // ctr+C to stop
 
